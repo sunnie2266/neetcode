9b2e21a8dec64059bedfde7a7dbe6e8c14745d60
Nguyen Dinh Tung Duong <quyquyetqq@gmail.com>
2026-09-07 00:07:46 +0700

    Add: sql-highest-grade-for-each-student - submission-0

---
+++ SQL/sql-highest-grade-for-each-student/submission-0.sql
@@ -0,0 +1,11 @@
+SELECT student_id, exam_id, score
+FROM (
+    SELECT 
+        student_id, 
+        exam_id, 
+        score,
+        ROW_NUMBER() OVER (PARTITION BY student_id ORDER BY score DESC, exam_id ASC) AS rn
+    FROM exam_results
+) AS ranked_table
+WHERE rn = 1
+ORDER BY student_id ASC;
