343afda42afe77a0cc7610b9b9151765145b5c3a
Nguyen Dinh Tung Duong <quyquyetqq@gmail.com>
2026-09-07 14:09:37 +0700

    Add: sql-customers-bought-a-b-not-c - submission-5

---
+++ SQL/sql-customers-bought-a-b-not-c/submission-5.sql
@@ -0,0 +1,1 @@
+SELECT customers.customer_id, customers.customer_name FROM customers WHERE customers.customer_id IN (SELECT orders.customer_id FROM orders WHERE product_name = 'A') AND customers.customer_id IN (SELECT customer_id FROM orders WHERE product_name = 'B') AND customers.customer_id NOT IN (SELECT customer_id FROM orders WHERE product_name = 'C') ORDER BY customer_name
